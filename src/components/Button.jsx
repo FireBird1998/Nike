2af048998b4a064989ff8d952ddabcfb818cdccb
Ankit Das <www.das5471@gmail.com>
2023-09-09 03:22:21 +0530

    Super quality section done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,11 +3,11 @@ const Button = ({lable, iconURL}) => {
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
         {lable}
 
-        <img
+       {iconURL && <img
          src={iconURL} 
          alt="Arrow Right" 
          className="ml-2 rounded-full w-5 h-5"
-        />
+        />}
 
     </button>
   )
