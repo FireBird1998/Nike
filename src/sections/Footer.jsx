c71591b91c43a6a39ac483a1aba489b4c2808162
Ankit Das <www.das5471@gmail.com>
2023-09-10 05:02:24 +0530

    lastely footer section made

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,6 +1,58 @@
+import { footerLogo } from "../assets/images"
+import { footerLinks, socialMedia } from "../constants"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col ">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img 
+              src={footerLogo}
+              alt="footer logo"
+              width={150}
+              height={46}
+
+
+            />
+          </a>
+          <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">GEt your best shoes today</p>
+          <div className="flex items-center gap-5 mt-8">
+            {socialMedia.map((icon) => (
+              <div key={icon.src} className="flex items-center justify-center w-12 h-12 bg-white rounded-full ">
+                <img 
+                  src={icon.src}
+                  alt={icon.alt}
+                  width={24}
+                  height={24}
+                  />
+              </div>
+            ))}
+
+          </div>
+        </div>
+
+        <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap  ">
+          {footerLinks.map((section) => (
+            <div key={section}>
+              <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6">{section.title}</h4>
+              <ul>
+                {section.links.map((link) => (
+                  <li key={link.name} className="mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray cursor-pointer">
+                    <a href={link.link}>{link.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+        <p className="text-sm font-montserrat">© 2022 firebird1998. All rights reserved.</p>
+        <p className="text-sm font-montserrat">Privacy Policy | Terms of Use</p>
+      
+      </div>
+    </footer>
   )
 }
 
