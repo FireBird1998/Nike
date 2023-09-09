5d991d46d2842f0a9e8fb891ef2a98407bd9e087
Ankit Das <www.das5471@gmail.com>
2023-09-10 04:34:19 +0530

    sign up section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
-const Button = ({lable, iconURL, backgroundColor, borderColor, textColor }) => {
+const Button = ({lable, iconURL, backgroundColor, borderColor, textColor, fullWidth }) => {
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
-    ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : 'bg-coral-red text-white border-coral-red'} rounded-full`}>
+    ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : 'bg-coral-red text-white border-coral-red'} rounded-full ${fullWidth && 'w-full'} `}>
 
       {lable}
 
